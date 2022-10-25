679064ed67a8696cf422bedb1b4ee8bff197371b
emon <emondas457@gmail.com>
2022-10-25 21:59:09 +0600

    get data by id

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 const express = require('express')
 const app = express()
 const port = 5000
+let cors = require('cors')
+const Course = require('./data/Courses.json')
+const detailsCourse = require('./data/DetailCourse.json')
 
+
+app.use(cors())
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
+
+app.get('/Course', (req, res) => {
+    res.send(Course)
+})
+
+
+app.get('/course/:_id', (req, res) => {
+    const getCourse = detailsCourse.filter(course => course.id == req.params._id)
+    res.send(getCourse)
+})
+
+app.get('/detailsCourse/:id', (req, res) => {
+    let DetailsCourseByUniqId = detailsCourse.find(Detailscourse => Detailscourse.course_id === req.params.id)
+    console.log(DetailsCourse)
+    res.send(DetailsCourseByUniqId)
+})
+
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
